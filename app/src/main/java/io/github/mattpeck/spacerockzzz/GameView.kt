808c439e4fcb7c4c53c0e2a687f41bffbef20c50
Matt Peck <mpeck.85@gmail.com>
2020-02-15 20:24:01 -0700

    Constant FPS & Player
      - GameView :: Game Loop runs at a Constant 30 fps
      - Player :: Create player class

--- app/src/main/java/io/github/mattpeck/spacerockzzz/GameView.kt
+++ app/src/main/java/io/github/mattpeck/spacerockzzz/GameView.kt
@@ -1,38 +1,78 @@
 package io.github.mattpeck.spacerockzzz
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
-class GameView(context:Context) : SurfaceView(context), Runnable {
+class GameView(context: Context) : SurfaceView(context), Runnable {
 
+    object GameConstants {
+        const val FRAMES_PER_SECOND: Int = 30
+        const val SKIP_TICKS: Int = 1000 / FRAMES_PER_SECOND
+    }
 
-    @Volatile var isPlaying:Boolean = false
-    private var mThread:Thread? = null
+    @Volatile var isPlaying: Boolean = false
+
+    private var thread: Thread? = null
+    private var sleepTime: Long = 0
+    private var nextGameTick: Long = System.currentTimeMillis()
+    private var currentFPS: Long = 0
+
+    private val paint: Paint = Paint()
+    private val surfaceHolder: SurfaceHolder = holder
+
+    private val player: Player = Player(context)
 
     override fun run() {
         while (isPlaying) {
+            val startTime: Long = System.currentTimeMillis()
+
+            // Game Loop
             update()
             draw()
             control()
+
+            // Calculate FPS
+            val totalTime = System.currentTimeMillis() - startTime
+            if (totalTime > 0) {
+                currentFPS = 1000 / totalTime
+            }
         }
     }
 
     private fun update() {
-
+        player.update()
     }
 
     private fun draw() {
-
+        if (surfaceHolder.surface.isValid) {
+            val canvas: Canvas = surfaceHolder.lockCanvas()
+            // Black Background
+            canvas.drawColor(Color.BLACK)
+            // FPS Text
+            paint.color = Color.WHITE
+            paint.textSize = 45f
+            canvas.drawText("FPS: $currentFPS", 20f, 40f, paint)
+//            canvas.drawBitmap(player.bitmap, player.x, player.y, paint)
+            surfaceHolder.unlockCanvasAndPost(canvas)
+        }
     }
 
     private fun control() {
-
+        nextGameTick += GameConstants.SKIP_TICKS
+        sleepTime = nextGameTick - System.currentTimeMillis()
+        if (sleepTime >= 0) {
+            Thread.sleep(sleepTime)
+        }
     }
 
     fun pause() {
         isPlaying = false
         try {
-            mThread?.join()
+            thread?.join()
         } catch (e:InterruptedException) {
             
         }
@@ -40,7 +80,7 @@ class GameView(context:Context) : SurfaceView(context), Runnable {
 
     fun resume() {
         isPlaying = true
-        mThread = Thread(this@GameView)
-        mThread?.start()
+        thread = Thread(this@GameView)
+        thread?.start()
     }
 }
---
+++ app/src/main/java/io/github/mattpeck/spacerockzzz/Player.kt
@@ -0,0 +1,18 @@
+package io.github.mattpeck.spacerockzzz
+
+import android.content.Context
+
+class Player(context: Context) {
+
+    //val bitmap: Bitmap by lazy { BitmapFactory.decodeResource(context.resources, R.drawable.player)}
+
+    var x: Float = 0f
+    var y: Float = 0f
+    var angle: Float = 0f
+    var speed: Float = 0f
+
+    fun update() {
+
+    }
+
+}
